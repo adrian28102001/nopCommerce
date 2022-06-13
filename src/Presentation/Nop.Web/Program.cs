cbf7df2f7f49d167e365a4cdc8ffac9cfcc1074e
Gherman Adrian <gherman.adrian2001@gmail.com>
2022-06-13 15:36:17 +0300

    Added json export folder to gitignore

--- src/Presentation/Nop.Web/Program.cs
+++ src/Presentation/Nop.Web/Program.cs
@@ -18,7 +18,6 @@ builder.Configuration.AddEnvironmentVariables();
 //Add services to the application and configure service provider
 builder.Services.ConfigureApplicationServices(builder);
 
-
 var app = builder.Build();
 
 //Configure the application HTTP request pipeline
